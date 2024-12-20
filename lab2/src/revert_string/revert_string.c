fce29ed259d006e1c0634b4cfc69c64a6eeb245f
Egor228zorro <120336430+Egor228zorro@users.noreply.github.com>
2024-12-20 21:14:26 +0000

    Добавил все изменения и новые файлы

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,19 @@
 #include "revert_string.h"
-
+#include <string.h>
 void RevertString(char *str)
 {
-	// your code here
+    int len = strlen(str);  // Находим длину строки
+    int i = 0, j = len - 1;
+
+    // Меняем символы с начала и конца строки
+    while (i < j)
+    {
+        char temp = str[i];  // Сохраняем символ с позиции i
+        str[i] = str[j];     // Копируем символ с позиции j на позицию i
+        str[j] = temp;       // Копируем сохраненный символ на позицию j
+
+        i++;  // Перемещаем указатель i вправо
+        j--;  // Перемещаем указатель j влево
+    }
 }
 
--- lab2/src/swap/swap.c
+++ lab2/src/swap/swap.c
@@ -2,5 +2,7 @@
 
 void Swap(char *left, char *right)
 {
-	// ваш код здесь
+	char temp = *left; // Сохраняем значение, на которое указывает left
+    *left = *right;    // Копируем значение из right в left
+    *right = temp;     // Восстанавливаем значение left в переменную right
 }
