6e1e4c68aa69b238391f8498e413924fa55d4258
Egor228zorro <120336430+Egor228zorro@users.noreply.github.com>
2025-04-12 08:03:17 +0000

    Добавил коментарии к файду factorial.c и mutex.c и изменил 1 задание 5 лабы,изменив выполение программы с мьютексом

--- lab5/src/factorial.c
+++ lab5/src/factorial.c
@@ -1,90 +1,99 @@
-#include <pthread.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <getopt.h>
-
+#include <pthread.h> // Для работы с потоками
+#include <stdio.h>   
+#include <stdlib.h>  
+#include <getopt.h>  
 typedef struct {
-    int start;
-    int end;
-    int mod;
-    long long* result;
-    pthread_mutex_t* mutex;
+    int start;           // Начало диапазона для вычисления частичного факториала
+    int end;             // Конец диапазона для вычисления частичного факториала
+    int mod;             // Модуль, по которому вычисляется факториал
+    long long* result;   // Указатель на общую переменную для хранения результата
+    pthread_mutex_t* mutex; // Указатель на мьютекс для синхронизации доступа к общей переменной
 } ThreadArgs;
 
+// Функция, выполняемая каждым потоком
 void* partial_factorial(void* args) {
-    ThreadArgs* targs = (ThreadArgs*)args;
-    long long partial_result = 1;
+    ThreadArgs* targs = (ThreadArgs*)args; // Приведение аргумента к типу ThreadArgs*
+    long long partial_result = 1;          // Инициализация частичного результата
 
+    // Вычисление частичного факториала
     for (int i = targs->start; i <= targs->end; ++i) {
-        partial_result = (partial_result * i) % targs->mod;
+        partial_result = (partial_result * i) % targs->mod; // Вычисление произведения по модулю
     }
 
+    // Захват мьютекса для безопасного обновления общей переменной result
     pthread_mutex_lock(targs->mutex);
-    *(targs->result) = (*(targs->result) * partial_result) % targs->mod;
-    pthread_mutex_unlock(targs->mutex);
+    *(targs->result) = (*(targs->result) * partial_result) % targs->mod; // Обновление общей переменной result
+    pthread_mutex_unlock(targs->mutex); // Освобождение мьютекса
 
-    return NULL;
+    return NULL; // Поток завершает свою работу
 }
 
 int main(int argc, char* argv[]) {
-    int k = -1, pnum = -1, mod = -1;
+    int k = -1, pnum = -1, mod = -1; // Инициализация переменных для аргументов командной строки
 
+    // Определение структуры для описания длинных опций командной строки
     const struct option long_options[] = {
-        {"k", required_argument, 0, 'k'},
-        {"pnum", required_argument, 0, 'p'},
-        {"mod", required_argument, 0, 'm'},
-        {0, 0, 0, 0}
+        {"k", required_argument, 0, 'k'},    // Опция --k с обязательным аргументом
+        {"pnum", required_argument, 0, 'p'}, // Опция --pnum с обязательным аргументом
+        {"mod", required_argument, 0, 'm'},  // Опция --mod с обязательным аргументом
+        {0, 0, 0, 0}                          // Конец массива опций
     };
 
-    int option_index = 0;
+    int option_index = 0; // Индекс текущей опции
+    // Разбор аргументов командной строки с использованием getopt_long
     while (1) {
-        int c = getopt_long(argc, argv, "k:p:m:", long_options, &option_index);
-        if (c == -1) break;
+        int c = getopt_long(argc, argv, "k:p:m:", long_options, &option_index); // Разбор опции
+        if (c == -1) break; // Если getopt_long вернул -1, значит, опций больше нет
 
         switch (c) {
-            case 'k': k = atoi(optarg); break;
-            case 'p': pnum = atoi(optarg); break;
-            case 'm': mod = atoi(optarg); break;
-            default: return 1;
+            case 'k': k = atoi(optarg); break;    // Опция -k: преобразование аргумента в целое число
+            case 'p': pnum = atoi(optarg); break; // Опция -p: преобразование аргумента в целое число
+            case 'm': mod = atoi(optarg); break;  // Опция -m: преобразование аргумента в целое число
+            default: return 1;                     // Обработка неизвестной опции
         }
     }
 
+    // Проверка валидности аргументов командной строки
     if (k <= 0 || pnum <= 0 || mod <= 0) {
         printf("Invalid arguments. Example usage: %s -k 10 --pnum=4 --mod=10\n", argv[0]);
-        return 1;
+        return 1; // Выход с ошибкой
     }
 
-    pthread_t threads[pnum];
-    ThreadArgs thread_args[pnum];
-    pthread_mutex_t mutex;
-    pthread_mutex_init(&mutex, NULL);
+    pthread_t threads[pnum];       // Объявление массива потоков
+    ThreadArgs thread_args[pnum];    // Объявление массива аргументов для потоков
+    pthread_mutex_t mutex;           // Объявление мьютекса
+    pthread_mutex_init(&mutex, NULL); // Инициализация мьютекса
 
-    long long result = 1;
-    int chunk_size = k / pnum;
-    int remainder = k % pnum;
+    long long result = 1; // Инициализация общей переменной для хранения результата
+    int chunk_size = k / pnum; // Вычисление размера куска для каждого потока
+    int remainder = k % pnum;  // Вычисление остатка от деления
 
+    // Создание потоков
     for (int i = 0; i < pnum; ++i) {
-        thread_args[i].start = i * chunk_size + 1;
-        thread_args[i].end = (i + 1) * chunk_size + (i == pnum - 1 ? remainder : 0);
-        thread_args[i].mod = mod;
-        thread_args[i].result = &result;
-        thread_args[i].mutex = &mutex;
+        // Заполнение структуры аргументов для потока
+        thread_args[i].start = i * chunk_size + 1; // Начало диапазона для текущего потока
+        thread_args[i].end = (i + 1) * chunk_size + (i == pnum - 1 ? remainder : 0); // Конец диапазона для текущего потока (последний поток обрабатывает остаток)
+        thread_args[i].mod = mod;             // Модуль
+        thread_args[i].result = &result;       // Указатель на общую переменную result
+        thread_args[i].mutex = &mutex;         // Указатель на мьютекс
 
+        // Создание потока
         if (pthread_create(&threads[i], NULL, partial_factorial, &thread_args[i]) != 0) {
-            perror("pthread_create");
-            return 1;
+            perror("pthread_create"); 
+            return 1;                
         }
     }
 
+    // Ожидание завершения потоков
     for (int i = 0; i < pnum; ++i) {
         if (pthread_join(threads[i], NULL) != 0) {
-            perror("pthread_join");
-            return 1;
+            perror("pthread_join"); 
+            return 1;                
         }
     }
 
-    pthread_mutex_destroy(&mutex);
+    pthread_mutex_destroy(&mutex); // Уничтожение мьютекса
 
-    printf("Result: %lld\n", result);
-    return 0;
+    printf("Result: %lld\n", result); 
+    return 0;                           
 }
--- lab5/src/mutex.c
+++ lab5/src/mutex.c
@@ -1,92 +1,97 @@
 /********************************************************
- * An example source module to accompany...
+ * Пример работы с мьютексами в многопоточном приложении
  *
- * "Using POSIX Threads: Programming with Pthreads"
- *     by Brad nichols, Dick Buttlar, Jackie Farrell
- *     O'Reilly & Associates, Inc.
- *  Modified by A.Kostin
- ********************************************************
- * mutex.c
- *
- * Simple multi-threaded example with a mutex lock.
+ * - создание потоков
+ * - разделение общей переменной
+ * - синхронизацию с помощью мьютекса
+ * - состояние гонки (при отключенном мьютексе)
  */
-#include <errno.h>
-#include <pthread.h>
-#include <stdio.h>
-#include <stdlib.h>
-
-void do_one_thing(int *);
-void do_another_thing(int *);
-void do_wrap_up(int);
-int common = 0; /* A shared variable for two threads */
-int r1 = 0, r2 = 0, r3 = 0;
-pthread_mutex_t mut = PTHREAD_MUTEX_INITIALIZER;
-
-int main() {
-  pthread_t thread1, thread2;
-
-  if (pthread_create(&thread1, NULL, (void *)do_one_thing,
-			  (void *)&common) != 0) {
-    perror("pthread_create");
-    exit(1);
-  }
-
-  if (pthread_create(&thread2, NULL, (void *)do_another_thing,
-                     (void *)&common) != 0) {
-    perror("pthread_create");
-    exit(1);
-  }
-
-  if (pthread_join(thread1, NULL) != 0) {
-    perror("pthread_join");
-    exit(1);
-  }
-
-  if (pthread_join(thread2, NULL) != 0) {
-    perror("pthread_join");
-    exit(1);
-  }
-
-  do_wrap_up(common);
-
-  return 0;
-}
-
-void do_one_thing(int *pnum_times) {
-  int i, j, x;
-  unsigned long k;
-  int work;
-  for (i = 0; i < 50; i++) {
-    // pthread_mutex_lock(&mut);
-    printf("doing one thing\n");
-    work = *pnum_times;
-    printf("counter = %d\n", work);
-    work++; /* increment, but not write */
-    for (k = 0; k < 500000; k++)
-      ;                 /* long cycle */
-    *pnum_times = work; /* write back */
-	// pthread_mutex_unlock(&mut);
-  }
-}
-
-void do_another_thing(int *pnum_times) {
-  int i, j, x;
-  unsigned long k;
-  int work;
-  for (i = 0; i < 50; i++) {
-    // pthread_mutex_lock(&mut);
-    printf("doing another thing\n");
-    work = *pnum_times;
-    printf("counter = %d\n", work);
-    work++; /* increment, but not write */
-    for (k = 0; k < 500000; k++)
-      ;                 /* long cycle */
-    *pnum_times = work; /* write back */
-    // pthread_mutex_unlock(&mut);
-  }
-}
-
-void do_wrap_up(int counter) {
-  int total;
-  printf("All done, counter = %d\n", counter);
-}
+ #include <errno.h>   // Для работы с кодами ошибок
+ #include <pthread.h> // Функции работы с потоками POSIX
+ #include <stdio.h>
+ #include <stdlib.h>
+ 
+ void do_one_thing(int *);   // функция для 1 потока
+ void do_another_thing(int *); // функция для 2 потока
+ void do_wrap_up(int);        // функция для завершения
+ int common = 0;              /* общая переменная для 2 потоков */
+ int r1 = 0, r2 = 0, r3 = 0;
+ pthread_mutex_t mut =
+     PTHREAD_MUTEX_INITIALIZER; // Инициализация мьютекса с помощью макроса
+ 
+ int main() {
+   pthread_t thread1, thread2; // идентификаторы потоков
+ 
+   // Создание первого потока
+   if (pthread_create(&thread1, NULL, (void *)do_one_thing,
+                      (void *)&common) != 0) {
+     perror("pthread_create"); 
+     exit(1);                  
+   }
+ 
+   // Создание второго потока
+   if (pthread_create(&thread2, NULL, (void *)do_another_thing,
+                      (void *)&common) != 0) {
+     perror("pthread_create"); 
+     exit(1);                  
+   }
+ 
+   // Ожидание завершения первого потока
+   if (pthread_join(thread1, NULL) != 0) {
+     perror("pthread_join"); 
+     exit(1);                  
+   }
+ 
+   // Ожидание завершения второго потока
+   if (pthread_join(thread2, NULL) != 0) {
+     perror("pthread_join"); 
+     exit(1);                  
+   }
+ 
+   // Вызов функции завершения
+   do_wrap_up(common);
+ 
+   return 0; // Успешное завершение программы
+ }
+ 
+ // Функция, выполняемая первым потоком
+ void do_one_thing(int *pnum_times) {
+   int i, j, x;
+   unsigned long k;
+   int work;
+   for (i = 0; i < 50; i++) {
+    pthread_mutex_lock(&mut); // Захват мьютекса (закомментировано ,если нужно для демонстрации состояния гонки)
+     printf("doing one thing\n"); // Вывод сообщения
+     work = *pnum_times;           // Чтение значения общей переменной
+     printf("counter = %d\n", work); // Вывод значения счетчика
+     work++;                       /* увеличение, но не запись */
+     for (k = 0; k < 500000; k++)
+       ;                     /* длинный цикл */
+     *pnum_times = work;   /* запись обратно */
+     pthread_mutex_unlock(&mut); // Освобождение мьютекса (закомментировано)
+   }
+ }
+ 
+ // Функция, выполняемая вторым потоком
+ void do_another_thing(int *pnum_times) {
+   int i, j, x;
+   unsigned long k;
+   int work;
+   for (i = 0; i < 50; i++) {
+     pthread_mutex_lock(&mut); // Захват мьютекса (надо закометить,если нужно состояние гонки)
+     printf("doing another thing\n"); 
+     work = *pnum_times;           // Чтение значения общей переменной
+     printf("counter = %d\n", work); // Вывод значения счетчика
+     work++;                       /* увеличение, но не запись */
+     for (k = 0; k < 500000; k++)
+       ;                     /* длинный цикл */
+     *pnum_times = work;   /* запись обратно */
+      pthread_mutex_unlock(&mut); // Освобождение мьютекса (надо закометить,если нужно состояние гонки)
+   }
+ }
+ 
+ // Функция завершения
+ void do_wrap_up(int counter) {
+   int total;
+   printf("All done, counter = %d\n", counter); // Вывод итогового значения счетчика
+ }
